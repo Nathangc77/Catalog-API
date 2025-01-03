0e1898737a3773d337c7100aa9c9d79c6b0e1a8d
Nathan Moreira <nathangc7@gmail.com>
2025-01-03 10:17:58 -0300

    Auditing data

--- backend/src/main/java/com/moreira/catalog/entities/Category.java
+++ backend/src/main/java/com/moreira/catalog/entities/Category.java
@@ -4,6 +4,8 @@ package com.moreira.catalog.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.Instant;
+
 @Entity
 @Table(name = "tb_category")
 @NoArgsConstructor
@@ -17,4 +19,20 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
+    }
 }
