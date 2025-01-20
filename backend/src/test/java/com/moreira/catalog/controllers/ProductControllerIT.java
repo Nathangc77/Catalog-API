05d18d3fea94990631828da45095bcca1d185c9d
Nathan Moreira <nathangc7@gmail.com>
2025-01-20 11:45:06 -0300

    feat: Tests passing with apply spring security

--- backend/src/test/java/com/moreira/catalog/controllers/ProductControllerIT.java
+++ backend/src/test/java/com/moreira/catalog/controllers/ProductControllerIT.java
@@ -3,6 +3,7 @@ package com.moreira.catalog.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.moreira.catalog.dtos.ProductDTO;
 import com.moreira.catalog.factory.Factory;
+import com.moreira.catalog.factory.TokenUtil;
 import com.moreira.catalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,10 +30,15 @@ public class ProductControllerIT {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private TokenUtil tokenUtil;
+
     private long existingId;
     private long nonExistingId;
     private long countTotalProducts;
     private ProductDTO productDTO;
+
+    private String username, password, bearerToken;
 
     @BeforeEach
     void setUp() throws Exception {
@@ -40,6 +46,11 @@ public class ProductControllerIT {
         nonExistingId = 100L;
         countTotalProducts = 25L;
         productDTO = Factory.createProductDTO();
+
+        username = "maria@gmail.com";
+        password = "123456";
+
+        bearerToken = tokenUtil.obtainAccessToken(mockMvc, username, password);
     }
 
     @Test
@@ -66,6 +77,7 @@ public class ProductControllerIT {
 
         ResultActions result =
                 mockMvc.perform(MockMvcRequestBuilders.put("/products/{id}", existingId)
+                        .header("Authorization", "Bearer " + bearerToken)
                         .content(jsonBody)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON));
@@ -83,6 +95,7 @@ public class ProductControllerIT {
 
         ResultActions result =
                 mockMvc.perform(MockMvcRequestBuilders.put("/products/{id}", nonExistingId)
+                        .header("Authorization", "Bearer " + bearerToken)
                         .content(jsonBody)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON));
